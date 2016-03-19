8d8702c2fd700d786a440ff1bc8b3f4a6033ebe6
NealHumphrey <humphrey.neal@gmail.com>
2016-03-19 11:56:56 -0400

    got a prep area set up with object cloning

--- js/stagePlot.js
+++ js/stagePlot.js
@@ -2,9 +2,55 @@
 window.onload = function() {
 // create a wrapper around native canvas element (with id="c")
 var stage = new fabric.Canvas('stage');
+var prep = new fabric.Canvas('prep');
+
+var prepRect = new fabric.Rect({left:10,top:10,fill:'red', width:130, height:50, cornerSize:5, padding:10, rx:3, ry:3, selectable:false})
+prep.add(prepRect);
 
 
-//example object - a rectangle
+//now load an amp
+var svgSrc = "svg/amp_nick_bluth.svg"
+var amp = fabric.loadSVGFromURL(svgSrc);
+
+
+    fabric.loadSVGFromURL(svgSrc,function(objects,options) {
+    	var obj = fabric.util.groupSVGElements(objects, options);
+ 		obj.set({
+                left: 10,
+                top: 100,
+                width:100,		//?? affects the control box size, not image
+                height:100,
+                selectable:false
+        });
+
+ 		prep.add(obj);
+    });
+
+stage.renderAll();
+prep.renderAll();
+//add some event listeners
+prep.on('mouse:down', function(options){
+	if(options.target) {
+		//TODO need to make it so that the object's group is cloned, if it has one (i.e. to include the object label)
+		//TODO error in the clone() function for SVG objects... - potential? http://stackoverflow.com/questions/29494372/fabric-js-clone-path-group-type-object
+		console.log('object clicked', options.target.type);
+		var newItem = options.target.clone();
+		newItem.set({top:50, left:50});
+		stage.add(newItem);
+		stage.renderAll();
+	}
+});
+
+};
+
+//resizing example for mobile responsive: http://stackoverflow.com/questions/28301286/scale-fabric-js-canvas-objects
+//putting limits on the outer border drag area example: http://stackoverflow.com/questions/19979644/set-object-drag-limit-in-fabric-js
+
+
+
+
+/*old examples
+//Adda a rectangle, a label and group them
 var rect = new fabric.Rect({
   left: 100,
   top: 100,
@@ -23,45 +69,19 @@ var rect = new fabric.Rect({
   selectable: true,		//default=true. Allows user to move
 });
 // "add" rectangle onto canvas
-stage.add(rect);
 
+var boxTitle = new fabric.Text('Neal', { 
+	left:100, 
+	top:300,
+	fontFamily: 'Arial',
+	fontSize: 16
+	});
 
-//now load an amp
-var svgSrc = "svg/amp_nick_bluth.svg"
-var amp = fabric.loadSVGFromURL(svgSrc);
+var labeledPerson = new fabric.Group([rect, boxTitle],{
+	left: 150,
+	top:100
+	});
 
+stage.add(labeledPerson);
 
-    fabric.loadSVGFromURL(svgSrc,function(objects,options) {
-    	var obj = fabric.util.groupSVGElements(objects, options);
- 		obj.set({
-                left: 250,
-                top: 100,
-                width:100,		//?? affects the control box size, not image
-                height:100
-        });
-
- 		stage.add(obj).renderAll();
-
- 		/*
-        var loadedObjects = new fabric.Group(group);
-
-        loadedObjects.set({
-                left: 200,
-                top: 100,
-                width:175,
-                height:175
-        });
-
-        canvas.add(loadedObjects);
-        canvas.renderAll();
-		*/
-    });
-
-    /*function(item, object) {
-            object.set('id',item.getAttribute('id'));
-            group.push(object);
-    });
-	*/
-};
-
-//resizing example for mobile responsive: http://stackoverflow.com/questions/28301286/scale-fabric-js-canvas-objects
+*/
